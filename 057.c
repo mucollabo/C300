34e72edf6ab162a76446f9d84b2a612b0e692817
Charles An <mucollabo@gmail.com>
2020-06-25 22:36:21 +0900

    c300 study until 060

---
+++ 057.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+#define KOREA "대한민국"
+
+int main(void)
+{
+    char *string1;
+    char string2[100];
+
+    // strcpy(string1, KOREA);
+    strcpy(string2, KOREA);
+    strcpy(string2, "봄");
+
+    // return 0;
+}
